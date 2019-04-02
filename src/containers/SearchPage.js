cb5ac4562b6bca563038112b814c00f83fb411f2
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-04-02 16:18:57 +0300

    add reselect

--- src/containers/SearchPage.js
+++ src/containers/SearchPage.js
@@ -10,6 +10,7 @@ import { searchMovies } from "../actions";
 import {
   getSearchMoviesSelector,
   getSearchMoviesCountSelector,
+  getSearchMoviesVoteAverageSelector,
   isSearchMoviesFetchingSelector,
   getMoviesSearchQuerySelector
 } from "../selectors";
@@ -51,13 +52,18 @@ class SearchPage extends Component {
   };
 
   render() {
-    const { search, loading, movies, moviesCount } = this.props;
+    const { search, loading, movies, moviesCount, voteAverage } = this.props;
     return (
       <div>
         <SearchBox search={search} onSearch={this.handleSearch} />
         <div style={styles.container}>
           <Loader loading={loading}>
-            {!!movies.length && <p>Found {moviesCount} movies</p>}
+            {!!movies.length && (
+              <div>
+                <p>Found {moviesCount} movies</p>
+                <p>Vote average {Math.round(voteAverage * 100) / 100}</p>
+              </div>
+            )}
             <MovieGrid movies={movies} />
           </Loader>
         </div>
@@ -70,6 +76,7 @@ function mapStateToProps(state, ownProps) {
   return {
     movies: getSearchMoviesSelector(state),
     moviesCount: getSearchMoviesCountSelector(state),
+    voteAverage: getSearchMoviesVoteAverageSelector(state),
     loading: isSearchMoviesFetchingSelector(state),
     search: getMoviesSearchQuerySelector(state)
   };
--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,15 +1,32 @@
+import { createSelector } from "reselect";
+
+export const getRouterLocationSelector = (state) => state.router.location;
+
 export const isLoggingInSelector = (state) => state.session.isLoggingIn;
 export const isLoggedInSelector = (state) => state.session.isLoggedIn;
 
 export const getAuthErrorSelector = (state) => state.session.error;
-export const getNextLocationSelector = (state) =>
-  state.router.location.state && state.router.location.state.nextLocation;
+export const getNextLocationSelector = createSelector(
+  getRouterLocationSelector,
+  (location) => location.state && location.state.nextLocation
+);
 
 export const getMovieSelector = (state) => state.movie.info;
 export const isMovieFetchingSelector = (state) => state.movie.isFetching;
 export const getMovieIdSelector = (state, props) => props.match.params.id;
 
 export const getSearchMoviesSelector = (state) => state.movies.items;
-export const getSearchMoviesCountSelector = (state) => getSearchMoviesSelector(state).length;
+export const getSearchMoviesCountSelector = createSelector(
+  getSearchMoviesSelector,
+  (movies) => movies.length
+);
+export const getSearchMoviesVoteAverageSelector = createSelector(
+  getSearchMoviesSelector,
+  getSearchMoviesCountSelector,
+  (movies, totalCount) => movies.reduce((sum, movie) => (sum += movie.voteAverage), 0) / totalCount
+);
 export const isSearchMoviesFetchingSelector = (state) => state.movies.isFetching;
-export const getMoviesSearchQuerySelector = (state) => state.router.location.query.search;
+export const getMoviesSearchQuerySelector = createSelector(
+  getRouterLocationSelector,
+  (location) => location.query.search
+);
